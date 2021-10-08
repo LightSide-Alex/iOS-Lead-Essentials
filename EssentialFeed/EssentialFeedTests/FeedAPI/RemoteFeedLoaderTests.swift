39d6903e55ca79aeafe630c8f1978249b05fc141
Alex Balan <alex.balan@tacitcorporation.com>
2021-10-07 23:00:31 -0400

    test CI

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -134,7 +134,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             "id": item.id.uuidString,
             "description": item.description,
             "location": item.location,
-            "image": item.image.absoluteString
+            "imageFail": item.image.absoluteString
         ].compactMapValues { $0 }
         
         return (item, jsonItem)
