3826a0e4aef30f4197dbe3294103405ba1528a19
Alex Balan <alex.balan@tacitcorporation.com>
2022-03-17 10:48:31 -0400

    Move `FeedLoadingViewModel` to a separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Oleksandr Balan on 2022-03-17.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
