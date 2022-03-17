13aeada9f803f828ecf5c3f98914315be9cacac0
Alex Balan <alex.balan@tacitcorporation.com>
2022-03-17 10:47:22 -0400

    Move FeedViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Oleksandr Balan on 2022-03-17.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
