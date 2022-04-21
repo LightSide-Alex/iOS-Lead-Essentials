b3de1a3602a3073ee4975a6156af6276d1a7f0e7
Alex Balan <alex.balan@tacitcorporation.com>
2022-04-20 20:29:17 -0400

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Oleksandr Balan on 2022-04-20.
+//
+
+import XCTest
+
+class EssentialAppUIAcceptanceTests: XCTestCase {
+}
