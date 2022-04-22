342dc66231d5fb0895db4f6dbcd0e477e5ddc6d1
Alex Balan <alex.balan@tacitcorporation.com>
2022-04-22 10:23:49 -0400

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness - We can now run UI tests without internet connection.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -11,11 +11,11 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
         
-        app.launchArguments = ["-reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
         app.launch()
         
         let cells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cells.count, 22)
+        XCTAssertEqual(cells.count, 2)
         
         let image = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(image.exists)
@@ -23,7 +23,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
-        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
         onlineApp.launch()
         
         let offlineApp = XCUIApplication()
@@ -32,7 +32,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
         offlineApp.launch()
         
         let cells = offlineApp.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cells.count, 22)
+        XCTAssertEqual(cells.count, 2)
         
         let image = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(image.exists)
