69b96f790ab73a7642d6db90ea95a98ba64cc2f8
Alex Balan <alex.balan@tacitcorporation.com>
2022-07-18 11:00:34 -0400

    Add "Load more" cell controller with loading indicator

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -28,6 +28,15 @@ class FeedSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
     }
     
+    func test_feedWithLoadMoreIndicator() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithLoadMoreIndicator())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_LOAD_MORE_INDICATOR_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_LOAD_MORE_INDICATOR_dark")
+    }
+    
     // MARK: - Helpers
     private func makeSUT() -> ListViewController {
         let bundle = Bundle(for: ListViewController.self)
@@ -69,12 +78,24 @@ class FeedSnapshotTests: XCTestCase {
         ]
     }
     
+    private func feedWithLoadMoreIndicator() -> [CellController] {
+        let stub = feedWithContent().last!
+        let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, onSelect: {})
+        stub.controller = cellController
+        
+        let loadMore = LoadMoreCellController()
+        loadMore.display(ResourceLoadingViewModel(isLoading: true))
+        return [
+            CellController(id: UUID(), dataSource: cellController),
+            CellController(id: UUID(), dataSource: loadMore)
+        ]
+    }
 }
 
 private extension ListViewController {
     func display(_ stubs: [ImageStub]) {
         let cells: [FeedImageCellController] = stubs.map { stub in
-            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub)
+            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, onSelect: {})
             stub.controller = cellController
             return cellController
         }
